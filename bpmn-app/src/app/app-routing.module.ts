7e1852098343f7fe1930edf84541946a251411c9
Teodora-Alexandra Dan <Teodora.Dan@msg.group>
2022-06-03 10:05:38 +0200

    Code editor setup

--- bpmn-app/src/app/app-routing.module.ts
+++ bpmn-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CodeComponent } from "./code/code.component";
+import { DiagramComponent } from "./diagram/diagram.component";
+import { HomeComponent } from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'code', component: CodeComponent },
+  { path: 'diagram', component: DiagramComponent },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bpmn-app/src/app/app.module.ts
+++ bpmn-app/src/app/app.module.ts
@@ -2,14 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { CodeComponent } from './code/code.component';
+import { DiagramComponent } from './diagram/diagram.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CodeComponent,
+    DiagramComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
